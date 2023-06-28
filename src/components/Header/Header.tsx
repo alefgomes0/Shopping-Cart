59c8a0de6963f4320666ae66fecd06fbbd8ab34b
unknown <119986066+alefgomes0@users.noreply.github.com>
2023-06-28 18:18:49 -0300

    Change Header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,23 +4,54 @@ export const Header = () => {
   return (
     <div className="header">
       <h1>BookStore</h1>
-      <div className="shop">
-        <NavLink to="/shop">
-          <h3>Shop</h3>
-        </NavLink>
+      <div className="header-links">
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/shop">Shop</NavLink>
         <h3>About</h3>
         <h3>Contact</h3>
       </div>
       <div className="cart-container">
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 256 256"
+          width="30"
+          height="30"
+          viewBox="0 0 512 512"
         >
+          <circle
+            cx="176"
+            cy="416"
+            r="16"
+            fill="none"
+            stroke="white"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="32"
+          />
+          <circle
+            cx="400"
+            cy="416"
+            r="16"
+            fill="none"
+            stroke="white"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="32"
+          />
           <path
-            fill="currentColor"
-            d="M220.61 60.16A6 6 0 0 0 216 58H53l-5.17-28.5A14 14 0 0 0 34.05 18H16a6 6 0 0 0 0 12h18a2 2 0 0 1 2 1.64l25.51 140.3a21.93 21.93 0 0 0 6.24 11.77a26 26 0 1 0 38.14 6.29h52.22A26 26 0 1 0 180 178H83.17a10 10 0 0 1-9.84-8.21L69.73 150H188.1a22 22 0 0 0 21.65-18.06l12.15-66.87a6 6 0 0 0-1.29-4.91ZM98 204a14 14 0 1 1-14-14a14 14 0 0 1 14 14Zm96 0a14 14 0 1 1-14-14a14 14 0 0 1 14 14Zm3.94-74.21a10 10 0 0 1-9.84 8.21H67.55L55.19 70h153.62Z"
+            fill="none"
+            stroke="white"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="32"
+            d="M48 80h64l48 272h256"
+          />
+          <path
+            fill="none"
+            stroke="white"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="32"
+            d="M160 288h249.44a8 8 0 0 0 7.85-6.43l28.8-144a8 8 0 0 0-7.85-9.57H128"
           />
         </svg>
       </div>
--- src/components/HomeContent/HomeContent.tsx
+++ src/components/HomeContent/HomeContent.tsx
@@ -1,3 +1,13 @@
 export const HomeContent = () => {
-  return <div>oi</div>;
+  return (
+    <div className="home-content">
+
+      <div className="image-container">
+        <img
+          src={process.env.PUBLIC_URL + "/images/anna-hunko-ajE5goOGzZc-unsplash.jpg"}
+          alt="a library"
+        />
+      </div>
+    </div>
+  )
 };
