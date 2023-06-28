1712c1a46f7b2eb2839b0a4558cb72db3d943fe4
unknown <119986066+alefgomes0@users.noreply.github.com>
2023-06-28 15:15:58 -0300

    Begin to create Header element

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+import { Header } from "./components/Header/Header";
+import { NoMatch } from "./components/NoMatch/NoMatch";
+import "./App.css";
+import { HomeContent } from "./components/HomeContent/HomeContent";
 
-function App() {
+export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomeContent />} />
+        <Route path="*" element={<NoMatch />} />
+      </Routes>
+    </>
   );
-}
+};
 
 export default App;
---
+++ src/components/NoMatch/NoMatch.tsx
@@ -0,0 +1,7 @@
+export const NoMatch = () => {
+  return (
+    <div className="no-match">
+      <h3>Page not found</h3>
+    </div>
+  );
+};
