f50e3c59b7422014e8b3bf80aeb53d0d0053ab33
unknown <119986066+alefgomes0@users.noreply.github.com>
2023-06-28 21:32:34 -0300

    Add more styles to landing page

--- src/components/HomeContent/HomeContent.tsx
+++ src/components/HomeContent/HomeContent.tsx
@@ -1,9 +1,12 @@
 export const HomeContent = () => {
   return (
     <div className="home-content">
-
       <div className="image-container">
+        <div className="cta">
+          <h2>You want, we have it</h2>
 
+          <button >Start shopping</button>
+        </div>
       </div>
     </div>
   )
